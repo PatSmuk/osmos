f11028983bb54728485cf8bb2ee2468c811916cd
Patman64 <patman256@yahoo.com>
2017-06-02 22:40:39 -0400

    Make the circles stay within the screen

--- main.lua
+++ main.lua
@@ -17,6 +17,22 @@ function love.update(dt)
     for i, circle in ipairs(circles) do
         circle.x = circle.x + circle.v_x * dt
         circle.y = circle.y + circle.v_y * dt
+
+        if circle.x - circle.r < 0 then
+            circle.x = 0 + circle.r
+            circle.v_x = -circle.v_x
+        elseif circle.x + circle.r > window_width then
+            circle.x = window_width - circle.r
+            circle.v_x = -circle.v_x
+        end
+
+        if circle.y - circle.r < 0 then
+            circle.y = 0 + circle.r
+            circle.v_y = -circle.v_y
+        elseif circle.y + circle.r > window_height then
+            circle.y = window_height - circle.r
+            circle.v_y = -circle.v_y
+        end
     end
 end
 
