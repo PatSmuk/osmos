2e54c35393f821ed4e9f432b27907d7e514b0780
Patman64 <patman256@yahoo.com>
2017-06-02 22:54:41 -0400

    Add comments

--- main.lua
+++ main.lua
@@ -3,13 +3,14 @@ local circles = {}
 local window_width = love.graphics.getWidth()
 local window_height = love.graphics.getHeight()
 
+-- Initialize 20 circles with random parameters.
 for i = 1, 20 do
     table.insert(circles, {
-        x = math.random(window_width),
-        y = math.random(window_height),
-        v_x = (math.random() - 0.5) * 50,
-        v_y = (math.random() - 0.5) * 50,
-        r = math.random(40) + 12,
+        x = math.random(window_width),      -- 1..(window width)
+        y = math.random(window_height),     -- 1..(window height)
+        v_x = (math.random() - 0.5) * 50,   -- (-25)..25
+        v_y = (math.random() - 0.5) * 50,   -- (-25)..25
+        r = math.random(40) + 12,           -- 13..53
         intersects = false
     })
 end
@@ -20,29 +21,41 @@ function circlesIntersect(c1, c2)
 end
 
 function love.update(dt)
+    -- For each circle...
     for i, circle in ipairs(circles) do
+        -- Reset the intersection status to false.
         circle.intersects = false
+
+        -- Update the position.
         circle.x = circle.x + circle.v_x * dt
         circle.y = circle.y + circle.v_y * dt
 
+        -- If we're too far left, put it back in the screen and move to the right.
         if circle.x - circle.r < 0 then
             circle.x = 0 + circle.r
             circle.v_x = -circle.v_x
+        -- If we're too far right...
         elseif circle.x + circle.r > window_width then
             circle.x = window_width - circle.r
             circle.v_x = -circle.v_x
         end
 
+        -- If we're too far up...
         if circle.y - circle.r < 0 then
             circle.y = 0 + circle.r
             circle.v_y = -circle.v_y
+        -- If we're too far down...
         elseif circle.y + circle.r > window_height then
             circle.y = window_height - circle.r
             circle.v_y = -circle.v_y
         end
 
+        -- For each circle (again)...
         for other_i, other_circle in ipairs(circles) do
+            -- Make sure this is one of the circles that comes before this one (so it doesn't get reset later).
             if other_i < i then
+                -- If they intersect, set the intersection status for this circle and the other.
+                -- Don't care if it's already intersecting a third circle here.
                 if circlesIntersect(circle, other_circle) then
                     circle.intersects = true
                     other_circle.intersects = true
@@ -53,12 +66,16 @@ function love.update(dt)
 end
 
 function love.draw()
+    -- For each circle...
     for i, circle in ipairs(circles) do
+        -- If it intersects another draw it red.
         if circle.intersects then
             love.graphics.setColor(239, 67, 67)
         else
+            -- Otherwise make it white.
             love.graphics.setColor(255, 255, 255)
         end
+        -- Draw the circle.
         love.graphics.circle("fill", circle.x, circle.y, circle.r)
     end
 end
